7f1018a55f12b8823b68239772d393bd570598c6
nangua <nangua.com>
2017-12-18 13:37:46 +0800

    zookeeper

--- src/main/java/com/test/net/zookeeper/TestZookeeper.java
+++ src/main/java/com/test/net/zookeeper/TestZookeeper.java
@@ -1,8 +1,50 @@
 package com.test.net.zookeeper;
 
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooDefs.Ids;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
+
 public class TestZookeeper {
 
-	public static void main(String[] args) {
-		
+	private static int sessionTimeout = 1000;
+
+	public static void main(String[] args) throws Exception {
+		String connectString = "localhost:" + 2181;
+		// 创建一个与服务器的连接
+		ZooKeeper zk = new ZooKeeper(connectString, sessionTimeout, new Watcher() {
+			// 监控所有被触发的事件
+			public void process(WatchedEvent event) {
+				System.out.println("已经触发了" + event.getType() + "事件！");
+			}
+		});
+		Stat stat = zk.exists("/testRootPath", true);
+		if (stat != null) {
+			System.out.println("/testRootPath 目录节点已经存在！");
+			zk.delete("/testRootPath/testChildPathOne", -1);
+			// zk.delete("/testRootPath/testChildPathTwo", -1);
+			zk.delete("/testRootPath", -1);
+		}
+		// 创建一个目录节点
+		zk.create("/testRootPath", "testRootData".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+		// 创建一个子目录节点
+		zk.create("/testRootPath/testChildPathOne", "childOneData".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+		// 取出子目录节点列表
+		System.out.println(new String(zk.getData("/testRootPath", false, null)));
+		// 修改子目录节点数据
+		zk.setData("/testRootPath/testChildPathOne", "modifyChildDataOne".getBytes(), -1);
+		System.out.println("目录节点状态：[" + zk.exists("/testRootPath", true) + "]");
+		// 创建另外一个子目录节点
+		zk.create("/testRootPath/testChildPathTwo", "childTwoData".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+		System.out.println(new String(zk.getData("/testRootPath/testChildPathTwo", true, null)));
+		// 删除子目录节点
+		zk.delete("/testRootPath/testChildPathTwo", -1);
+		zk.delete("/testRootPath/testChildPathOne", -1);
+		// 删除父目录节点
+		zk.delete("/testRootPath", -1);
+		// 关闭连接
+		zk.close();
 	}
 }
